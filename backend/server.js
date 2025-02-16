0e63fb1181cbccea5d2627f80b01b57ae0f13e59
Matheus Gouvea <matheus.gouvea360@gmail.com>
2025-02-16 00:55:33 -0300

    CORS5

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 // Lista de origens permitidas
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://frontend-nine-phi-54.vercel.app/'  // Adicione seu domínio da Vercel aqui
+  'https://gerenciamento-de-tarefas-to-do-list.vercel.app'  // Adicione seu domínio da Vercel aqui
 ];
 
 // Configuração CORS
